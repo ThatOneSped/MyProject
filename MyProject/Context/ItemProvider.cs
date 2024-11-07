65272f2e877eccb72f618e7f8c1314305a22c781
ThatOneSped <jackdgn46@yahoo.com>
2024-11-07 11:04:22 +0000

    added comments

--- MyProject/Context/ItemProvider.cs
+++ MyProject/Context/ItemProvider.cs
@@ -8,30 +8,30 @@ namespace MyProject.Context
 {
     public class ItemProvider
     {
-        private readonly DatabaseContext _context;
+        private readonly DatabaseContext _context; //dependency injection for dbcontext
 
         public ItemProvider(DatabaseContext context)
         {
             _context = context;
         }
 
-        public async Task<List<Item>> GetItemsAsync()
+        public async Task<List<Item>> GetItemsAsync() //method to get all items and their category
         {
             return await _context.Items.Include(i => i.Category).ToListAsync();
         }
 
-        public async Task<Item?> GetItemByIdAsync(int id)
+        public async Task<Item?> GetItemByIdAsync(int id) //method to get an item and its category by its items ID
         {
             return await _context.Items.Include(i => i.Category).FirstOrDefaultAsync(i => i.ID == id);
         }
 
-        public async Task AddItemAsync(Item item)
+        public async Task AddItemAsync(Item item) //method to create an item to the database
         {
             _context.Items.Add(item);
             await _context.SaveChangesAsync();
         }
 
-        public async Task DeleteItemAsync(int id)
+        public async Task DeleteItemAsync(int id) //method to delete item from database
         {
             var item = await _context.Items.FindAsync(id);
             if (item != null)
@@ -41,7 +41,7 @@ namespace MyProject.Context
             }
         }
 
-        public async Task UpdateItemAsync(Item item)
+        public async Task EditItemAsync(Item item) //method to edit item
         {
             _context.Items.Update(item);
             await _context.SaveChangesAsync();
